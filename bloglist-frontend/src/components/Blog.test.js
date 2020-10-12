9878cb6fcea87d6c4265dea93f76c9f72aaf0842
Miska23 <miskalinden@gmail.com>
2020-10-12 22:22:41 +0300

    exercise 5.16 done

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { fireEvent, render } from '@testing-library/react'
-import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 import BlogForm from './BlogForm'
 
@@ -15,86 +14,83 @@ const blog = {
     name: 'Testi testinen',
   }
 }
+describe('<Blog />', () => {
 
-test('renders content', () => {
-
-  const component = render(
-    <Blog blog={blog} />
-  )
-
-  //  component.debug()
-
-  const div = component.container.querySelector('.blog')
-  expect(div).toHaveTextContent(
-    'All about testing'
-  )
-})
-
-test('more information about a blog is displayed after clicking button once', async () => {
-
-  const component = render(
-    <Blog blog={blog} />
-  )
-
-  const button = component.container.querySelector('.expand-button')
-
-  fireEvent.click(button)
-
-  const div = component.container.querySelector('.blog-expanded')
-  expect(div).toHaveTextContent(
-    'www.kaikkikoodista.com'
-  )
-  expect(div).toHaveTextContent(
-    'Likes: 20'
-  )
-})
-
-test('clicking "like" button twice calls the event handler twice', async () => {
+  let component
 
   const mockHandler = jest.fn()
 
-  const component = render(
-    <Blog blog={blog} update={mockHandler}/>
-  )
+  beforeEach(() => {
+    component = render(
+      <Blog blog={blog} update={mockHandler}/>
+    )
+  })
 
-  const expandButton = component.container.querySelector('.expand-button')
+  test('renders content', () => {
 
-  fireEvent.click(expandButton)
+    const div = component.container.querySelector('.blog')
+    expect(div).toHaveTextContent(
+      'All about testing'
+    )
+  })
 
-  const likeButton = component.container.querySelector('.like-button')
+  test('more information about a blog is displayed after clicking button once', async () => {
 
-  fireEvent.click(likeButton)
-  fireEvent.click(likeButton)
+    const button = component.container.querySelector('.expand-button')
 
-  expect(mockHandler.mock.calls).toHaveLength(2)
+    fireEvent.click(button)
 
+    const div = component.container.querySelector('.blog-expanded')
+    expect(div).toHaveTextContent(
+      'www.kaikkikoodista.com'
+    )
+    expect(div).toHaveTextContent(
+      'Likes: 20'
+    )
+  })
+
+  test('clicking "like" button twice calls the event handler twice', async () => {
+
+    const expandButton = component.container.querySelector('.expand-button')
+
+    fireEvent.click(expandButton)
+
+    const likeButton = component.container.querySelector('.like-button')
+
+    fireEvent.click(likeButton)
+    fireEvent.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+
+  })
 })
 
-test('<BlogForm /> updates parent state and calls onSubmit', () => {
-  const createNewBlog = jest.fn()
+describe('<BlogForm /> ', () => {
 
-  const component = render(
-    <BlogForm createNewBlog={createNewBlog} />
-  )
+  test('creates a new blog with given input', () => {
+    const createNewBlog = jest.fn()
 
-  const titleInput = component.container.querySelector('#input-title')
-  const authorInput = component.container.querySelector('#author-title')
-  const urlInput = component.container.querySelector('#url-title')
-  const form = component.container.querySelector('#form-blog')
+    const component = render(
+      <BlogForm createNewBlog={createNewBlog} />
+    )
 
-  fireEvent.change(titleInput, {
-    target: { value: 'Front end testing is difficult' }
+    const titleInput = component.container.querySelector('#input-title')
+    const authorInput = component.container.querySelector('#input-author')
+    const urlInput = component.container.querySelector('#input-url')
+    const form = component.container.querySelector('#form-blog')
+
+    fireEvent.change(titleInput, {
+      target: { value: 'Front end testing is difficult' }
+    })
+    fireEvent.change(authorInput, {
+      target: { value: 'Test Tester' }
+    })
+    fireEvent.change(urlInput, {
+      target: { value: 'www.testers.com' }
+    })
+    fireEvent.submit(form)
+
+    expect(createNewBlog.mock.calls).toHaveLength(1)
+    expect(createNewBlog.mock.calls[0][0].author).toBe('Test Tester')
   })
-  fireEvent.change(authorInput, {
-    target: { value: 'Test Tester' }
-  })
-  fireEvent.change(urlInput, {
-    target: { value: 'www.testers.com' }
-  })
-  fireEvent.submit(form)
-
-  console.log(prettyDOM(form))
-
-  expect(createNewBlog.mock.calls).toHaveLength(1)
-  expect(createNewBlog.mock.calls[0][1].content).toBe('Test Tester')
 })
